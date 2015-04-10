cdd700d66e3d0ea3be3fb694531a6532bad4f597
mengfei <mengfei@yahoo-inc.com>
2015-04-10 01:13:39 -0700

    4-10

---
+++ gas-station.java
@@ -0,0 +1,17 @@
+public int canCompleteCircuit(int[] gas, int[] cost) {
+	// sum用于找不符合accumlation>0的点，那么默认让新的start点＝i+1
+	int sum = 0;
+	int result = -1;
+	// total是积累总数
+	int total = 0;
+
+	for (int i = 0; i < gas.length; i++) {
+		sum += gas[i] - cost[i];
+		total += gas[i] - cost[i];
+		if (sum < 0) {
+			result = i;
+			sum = 0;
+		}
+	}
+	return total >= 0 ? result + 1 : -1;
+}
