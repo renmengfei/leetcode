d16b17dbc0b4c84a746e9c8360f26e42e93a250e
mengfei <mengfei@yahoo-inc.com>
2015-04-28 00:56:41 -0700

    4-28

---
+++ count-prime.java
@@ -0,0 +1,24 @@
+//http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+public class Solution {
+    public int countPrimes(int n) {
+        if(n<=2) return 0;
+        
+        boolean[] notprime = new boolean[n+1]; // initial all are prime
+        
+        // 跳过2的倍数
+        for(int i=3;i<Math.sqrt(n);i+=2){
+            if(!notprime[i]){
+                for(int j= i*i; j<n;j+=i){
+                    notprime[j]=true;
+                }
+            }
+        }
+        
+        //count
+        int sum = 1; // 2 is prime
+        for (int i = 3; i < n; i += 2){
+            sum += (notprime[i]==false) ? 1 : 0;
+        }
+        return sum;
+    }
+}
