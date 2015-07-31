2857cb2b44241282cbf7904fa6414718c20b43af
mengfei <mengfei@yahoo-inc.com>
2015-07-31 00:14:04 -0700

    7-31

--- remove-duplicates-from-sorted-array.java
+++ remove-duplicates-from-sorted-array.java
@@ -4,7 +4,8 @@ public class Solution {
         int len = A.length;
         if(len==0) return 0;
         if(len==1) return 1;
-        
+
+	// end 初始化不能等于0，否则[1,1] return 0         
         int end=1;
         int pivot=A[0];
         for(int i=1;i<A.length;i++){
