8c700c061f284db30462b60273b050d8b885f870
mengfei <mengfei@yahoo-inc.com>
2015-10-11 22:26:04 -0700

    10-1

--- largest-rectangle-in-histogram.java
+++ largest-rectangle-in-histogram.java
@@ -12,7 +12,7 @@ public class Solution {
         int max = 0;
         for (int i = 0; i <= height.length; i++) {
             int curt = (i == height.length) ? -1 : height[i];
-            while (!stack.isEmpty() && curt < height[stack.peek()]) {
+            while (!stack.isEmpty() && curt <= height[stack.peek()]) {
                 int h = height[stack.pop()];
                 int w = stack.isEmpty() ? i : i - stack.peek() - 1;
                 max = Math.max(max, h * w);
