f2755e2a781cdd535051e51e040e8cae45757079
mengfei <mengfei@yahoo-inc.com>
2015-12-08 00:14:58 -0800

    12-8

--- reverse-bits.java
+++ reverse-bits.java
@@ -1,3 +1,4 @@
+// Method 1:
 public class Solution {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
@@ -8,3 +9,12 @@ public class Solution {
 		return result;
     }
 }
+
+// Method2: merge sort,减少时间if called multi-times
+        n = (n >> 16) | (n << 16);
+        n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8);
+        n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4);
+        n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2);
+        n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1);
+        return n;
+
