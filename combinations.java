9b5887d8388e6c27b2f97e70fb99a2cf34110afc
mengfei <mengfei@yahoo-inc.com>
2015-12-16 00:26:00 -0800

    12-16

--- combinations.java
+++ combinations.java
@@ -1,3 +1,27 @@
+// Method 1:模板题
+public class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        
+        helper(result, new ArrayList<Integer>(), n, k, 1);
+        return result; 
+    }
+    
+    public void helper(List<List<Integer>> result, List<Integer> path, int n, int k, int start){
+        if(path.size()==k){
+            result.add(new ArrayList<Integer>(path));
+            return;
+        }
+        
+        for(int i=start; i<=n; i++){
+            path.add(i);
+            helper(result, path, n, k, i+1);
+            path.remove(path.size()-1);
+        }
+    }
+}
+
+// Method 2: math
 public class Solution {
     //Cn(k)=Cn-1(k)+Cn-1(k-1)
     public List<List<Integer>> combine(int n, int k) {
